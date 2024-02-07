1a8f06fb5f8106612eb6342b34061ad64d057e3d
Ivan Frias <friasgilivan@gmail.com>
2024-02-07 18:04:28 +0100

    Creamos los servicios y el parent

---
+++ orders-service/src/test/java/com/ivanfrias/store/ordersservice/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanfrias.store.ordersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
